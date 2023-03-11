af5f7fb1f95af88714ff43decf76e646925fa027
branhoff <branhoff2@gmail.com>
2023-03-10 20:06:47 -0800

    adding docstrings for Customer

--- Customer.cpp
+++ Customer.cpp
@@ -1,5 +1,18 @@
 #include "Customer.h"
 
+/**
+ * @brief Creates a new Customer object with the given ID, first name, and last name.
+ *
+ * @pre None.
+ *
+ * @post A new Customer object is created with the given ID, first name, and last name.
+ *
+ * @param id The unique ID of the customer.
+ * @param fname The first name of the customer.
+ * @param lname The last name of the customer.
+ *
+ * @return None.
+ */
 Customer::Customer(string id, string fname, string lname)
 {
    this->id = id;
--- Customer.h
+++ Customer.h
@@ -8,6 +8,19 @@ class Customer
 private:
    
 public:
+   /**
+    * @brief Creates a new Customer object with the given ID, first name, and last name.
+    *
+    * @pre None.
+    *
+    * @post A new Customer object is created with the given ID, first name, and last name.
+    *
+    * @param id The unique ID of the customer.
+    * @param fname The first name of the customer.
+    * @param lname The last name of the customer.
+    *
+    * @return None.
+    */
    Customer(string id, string fname, string lname);
 
    string id;
