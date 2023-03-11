bbb4de3911b43c759fa4be358788e188386b5ef8
branhoff <branhoff2@gmail.com>
2023-03-10 21:10:34 -0800

    adding docstrings for Transaction class

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,5 +1,14 @@
 #include "Transaction.h"
 
+/**
+ * Creates a new transaction object with the given customer, item, and transaction type.
+ *
+ * @param customer A pointer to the customer associated with the transaction.
+ * @param item A pointer to the item involved in the transaction.
+ * @param type A string representing the type of the transaction (e.g., "borrow" or "return").
+ * @pre The customer and item pointers must be valid and not null.
+ * @post A new Transaction object is created with the given customer, item, and type.
+ */
 Transaction::Transaction(Customer* customer, Item* item, string type)
 {
    this->customer = customer;
--- Transaction.h
+++ Transaction.h
@@ -3,6 +3,15 @@
 #include "Customer.h"
 #include "Item.h"
 
+/**
+ * Creates a new transaction object with the given customer, item, and transaction type.
+ *
+ * @param customer A pointer to the customer associated with the transaction.
+ * @param item A pointer to the item involved in the transaction.
+ * @param type A string representing the type of the transaction (e.g., "borrow" or "return").
+ * @pre The customer and item pointers must be valid and not null.
+ * @post A new Transaction object is created with the given customer, item, and type.
+ */
 class Transaction
 {
    
